a8096304f485f18e9d4054f1ab5d591f912bfac4
MyrrdinAlsatian <stephan.jeanba@gmail.com>
2018-12-10 22:58:57 +0100

    add boucle de rendu + explication

--- wp-theme-vierge/front-page.php
+++ wp-theme-vierge/front-page.php
@@ -2,6 +2,8 @@
 get_header(); // recupération du fichier header.php
 ?>
 
+<!-- Ici ce retrouvera le contenu de la page d'accueil -->
+
 
 <img src="<?= get_stylesheet_directory_uri();
 
@@ -30,8 +32,41 @@ get_header(); // recupération du fichier header.php
    endwhile;
                 ?>
 
-    <!-- Ici ce retrouvera le contenu de la page d'accueil -->
+        <!-- wp_query permet de faire des boucles hors de la boucle de rendu -->
+         <?php 
+                $args = array( 'orderby' => 'date', 'posts_per_page' => '2' );
+                $the_query = new WP_Query ( $args );
+                     while ( $the_query -> have_posts() ) :
+                         $the_query -> the_post(); 
+         ?>
 
+         <?php
+                        the_post_thumbnail(null,['class'=> 'img-fluid']);
+                     // ce truc ca permet de récuperer une image mise en avant et lui attribuer une classe
+                     // si tu a un doute look la doc https://developer.wordpress.org/reference/functions/the_post_thumbnail/
+         ?>
+                </div>
+                <div class="detais">
+                    <?= custome_the_category() ?>
+
+                        <!-- <li><?php // the_category(null,'')?></li> -->
+                        
+                        <!-- Sa ca sert a récupérer les catégorie  -->
+
+
+                    <a href="<?php the_permalink() ?> ">     <!-- on récupère le lien vers le contenu -->
+
+                        <h4><?php the_title() ?></h4>
+                    </a>
+                    <?php the_excerpt()?> <!--  La on va sortir l'extrait du texte -->
+                    <p class="date"><?php the_time( get_option( 'date_format' ) ); ?></p>
+                </div>
+            </div>
+            <?php
+             endwhile;                
+			wp_reset_postdata(); 
+
+				?>
 <?php
 
     get_footer(); //récupération du fichier footer.php
